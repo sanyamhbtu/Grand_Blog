fadab7cd7ec043d93e1fbcd872c001a6a08d6d5f
sanyam <sanyamjainhbtu@gmail.com>
2024-06-26 08:17:52 +0530

    add Authentication services

---
+++ src/appwrite/auth.js
@@ -0,0 +1,52 @@
+import config from "../config.js"
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService{
+    client = new Client();
+    account;
+    constructor(){
+        this.client
+        .setEndpoint('config.appwrite_end_url') 
+        .setProject('config.appwrite_PROJECT_ID');
+        this.account = new Account(this.client)
+    }
+    async createAccount({email , password , name}){
+        try {
+            const userAccount = await this.account.create(ID.unique() ,email, password, name);
+            if (userAccount) {
+                // calling another function and direct logining into the account
+                return this.login({email , password});
+                
+            } else {
+                return userAccount;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+    async login({email, password}){
+        try {
+            return this.account.createEmailPasswordSession(email, password);
+        } catch (error) {
+            return error;
+        }
+    }
+    async currentUser(){
+      try {
+        const user = await this.account.get();
+        return user;
+      } catch (error) {
+        return error;
+      }
+      return null;
+    }
+    async logOut (){
+        try {
+            const result = await this.account.deleteSessions();
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+const authService = new AuthService();
+export default authService;
