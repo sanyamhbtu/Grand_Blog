e5406792a2f4733901c81f55412628de09cc30c3
sanyam <sanyamjainhbtu@gmail.com>
2024-06-28 09:33:56 +0530

    Almost complete project

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,21 @@ import footer from "./footer/footer"
 import header from "./header/header"
 import Logo from "./Logo"
 import LogoutBtn from "../components/header/LogoutBtn"
+import RTE from "./RTE"
+import Signup from "./Signup"
+import Login from "./Login"
+import PostForm from "./post-form/PostForm"
+import PostCard from "./PostCard"
+import AuthLayout from "./AuthLayout"
 export {
     footer,
     header,
     container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+    AuthLayout
 }
